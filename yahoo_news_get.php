8c4989d333236904db944b7653a043793387fff2
ChoChun <tigger19870528@hotmail.com>
2015-06-02 11:14:57 +0800

    新聞摘要更完整

--- yahoo_news_get.php
+++ yahoo_news_get.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
-<?php
-
-?>
 <html>
 <head>
     <meta charset="utf-8">
 </head>
+<body>
+<form action="yahoo_news_get.php" method="post">
+<h2>關鍵字： <input type='text' name='keyword' value='' />
+<input type='submit' value='確認' /></h2>
+</form>
+<br>
+<h2>yahoo 新聞標題摘要</h2>
 <?php
-$key_word='王建民';
+if($_POST['keyword']==null) exit;
+$key_word=$_POST['keyword'];
 $key_word=urlencode($key_word);                                                              
 $html=file_get_contents("https://tw.news.search.yahoo.com/search?p=$key_word&fr=uh3_news_web_gs");
     preg_match_all ( '#<div class="compText mt-5" ><p class="">(.+?)</p>#', $html, $parts );
@@ -18,4 +23,5 @@ echo $contect;
 echo "<br>";
 }
 ?>
+</body>
 </html>
