9a7dd6a950214ed34b5005b5cb95e9ff3f529c83
ChoChun <tigger19870528@hotmail.com>
2015-07-26 22:35:51 +0800

    1、特質改寫成功!!!!

--- savefight.php
+++ savefight.php
@@ -4,8 +4,17 @@ require_once "config/db_connect.php";//連結到資料庫taiwan_future
 //sleep(5);
 
 	if(isset($_POST['winner_array']) && !empty($_POST['winner_array'])){
+		if(!empty($_POST['opinion'])){
+			//echo "something";
+			//防注入?
+			$newopinion=$_POST['opinion'];
+			$query="insert into personality (`content`) value('" . $newopinion . "')";
+			mysql_query($query);
+		}		
+		$selected=$_POST['selected'];
 		$winner=$_POST['winner_array'];
 		$loser=$_POST['loser_array'];
+		$selected=json_decode($selected);
 		$winner=json_decode($winner);
 		$loser=json_decode($loser);
 		$thewinner=$winner[count($winner)-1];
@@ -13,6 +22,26 @@ require_once "config/db_connect.php";//連結到資料庫taiwan_future
 		mysql_query($query);
 		$roundId=mysql_insert_id();
 		
+		//讀取selected 的id
+		$opinionstring="";
+		for($s=0; $s<count($selected); $s++){
+			if($s==0) $opinionstring="`content`='" . $selected[$s] . "'";
+			else $opinionstring="`content`='" . $selected[$s] . "' OR " . $opinionstring;
+		}
+		$result=mysql_query("select id from personality where ($opinionstring)");
+		$selectedId=array();
+		if(mysql_num_rows($result)){
+			for($m=0; $m<mysql_num_rows($result); $m++){
+				$rowresult=mysql_fetch_row($result);
+				array_push($selectedId ,$rowresult[0]);
+			}
+		}
+		//存入fight_personality
+		for($i=0; $i<count($selectedId); $i++){
+			$query="insert into fight_personality (`round_id`, `personality_id`) value($roundId, $selectedId[$i])";
+			mysql_query($query);
+		}	
+		//存入fight_process
 		for($i=1; $i<count($winner); $i++){
 			$query="insert into fight_process (`round_id`, `winner_id`, `loser_id`) value($roundId, $winner[$i], $loser[$i])";
 			mysql_query($query);
